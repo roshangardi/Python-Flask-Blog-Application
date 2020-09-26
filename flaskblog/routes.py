f7aee907ea0d262bd1a440f7a384ca265ad5ca99
Roshan <roshan.gardi002@gmail.com>
2020-09-26 13:31:54 +0530

    User Account and Profile Picture

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,9 @@
+import secrets,os
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
+from PIL import Image
 from flask_login import login_user, current_user, logout_user, login_required
 
 posts_list = [
@@ -68,13 +70,30 @@ def logout():
     return redirect(url_for('home'))
 
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static','profile_pics',picture_fn)
+
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
 @app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
         current_user.username = form.username.data
-        current_user.email = form.email.email
+        current_user.email = form.email.data
         db.session.commit()
         flash('Your account has been updated!', 'success')
         return redirect(url_for('account'))
